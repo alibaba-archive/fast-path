6ae8b0519ef4296e3ccc470fa6198ff9bc9b95df
dead-horse <dead_horse@qq.com>
2014-09-09 22:35:15 +0800

    add benchmark result

--- benchmark/index.js
+++ benchmark/index.js
@@ -37,3 +37,17 @@ suite
   process.exit(0);
 })
 .run({ 'async': false });
+
+// node version: v0.11.13, date: Tue Sep 09 2014 22:33:32 GMT+0800 (CST)
+// Starting...
+// 2 tests completed.
+
+// path.extname("/abcde/../xxx/yyy/zzz.js")  x 1,154,030 ops/sec ±1.25% (95 runs sampled)
+// fpath.extname("/abcde/../xxx/yyy/zzz.js") x 9,036,275 ops/sec ±0.94% (98 runs sampled)
+//
+// node version: v0.10.31, date: Tue Sep 09 2014 22:32:44 GMT+0800 (CST)
+// Starting...
+// 2 tests completed.
+
+// path.extname("/abcde/../xxx/yyy/zzz.js")  x 1,255,161 ops/sec ±0.78% (98 runs sampled)
+// fpath.extname("/abcde/../xxx/yyy/zzz.js") x 7,580,940 ops/sec ±0.83% (93 runs sampled)
